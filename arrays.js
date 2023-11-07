e193a239be7fe83e52d2fe7f6c35505131b910e4
cesaryraziel <cesario.fcf@hotmail.com>
2023-11-07 16:39:00 -0300

    aprendendo js na pratica

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var lista = ['Pera', 'Uva', 'Jaca', 'Mamão', 'Melancia']
+
+var cont = 0
+
+while (cont < lista.length) {
+    console.log(lista[cont])
+    cont++
+    
+}
---
+++ exemplo_for.js
@@ -0,0 +1,9 @@
+// incremento
+for (let cont = 0; cont < 10; cont++) {
+    console.log(cont)
+}
+
+// decremento
+//for (let cont = 10; cont > 1; cont--) {
+    //console.log(cont)
+//}
