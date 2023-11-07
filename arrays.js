551e968606177b79ac503ff2ed0db20e825050fc
cesaryraziel <cesario.fcf@hotmail.com>
2023-11-07 17:44:05 -0300

    aprendendo na pratica

--- arrays.js
+++ arrays.js
@@ -3,7 +3,8 @@ var lista = ['Pera', 'Uva', 'Jaca', 'Mamão', 'Melancia']
 var cont = 0
 
 while (cont < lista.length) {
-    console.log(lista[cont])
+   // console.log(lista[cont])
     cont++
-    
-}
+   
+} for (let index = 0; index < lista.length; index++)
+     console.log(`${index+1} - ${lista[index]}`)
---
+++ cadastrar_aluno.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')()
+
+var nome, continuar, confirmarSaida
+var nomeAlunos = [] //Arrays
+
+continuar = true
+
+while (continuar) {
+    nome = prompt('Informe o nome do aluno: ')
+    nomeAlunos.push(nome) //inserir elementos no array
+
+    confirmarSaida = prompt('Desejar continuar? S - Sim | N - Não \n ')
+    if (confirmarSaida.toUpperCase() === 'N') {
+        continuar = false
+    }
+    
+}
+
+for (let index = 0; index < nomeAlunos.length; index++) {
+    console.log(nomeAlunos[index])
+}
